bf9d7b97a056ec66e2f4c000d9be6ce2eca1dfdf
Federico Donner <federico.donner@gmail.com>
2020-02-16 00:47:01 -0300

    fix(pastillero.php): populate dosis with correct drug name

--- src/routes/pastillero.php
+++ src/routes/pastillero.php
@@ -31,7 +31,7 @@ $app->get('/api/pastillero', function (Request $request, Response $response) {
 // Devuelve todas las drogas y sus medicinas
 $app->get('/api/pastillero/{id}', function (Request $request, Response $response) {
     try {
-      $id = $request->getAttribute('id');
+        $id = $request->getAttribute('id');
         $sql = "SELECT * FROM pastillero WHERE id = $id";
         // Get db object
         $db = new db();
@@ -43,31 +43,31 @@ $app->get('/api/pastillero/{id}', function (Request $request, Response $response
         $pastilleros = $stmt->fetchAll(PDO::FETCH_OBJ);
         $pastillero = $pastilleros[0];
 
-$sql = "SELECT * FROM dosis WHERE pastillero_id = $id";
+        $sql = "SELECT * FROM dosis WHERE pastillero_id = $id";
 
+        $stmt = $db->query($sql);
+        $dosis = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+        foreach ($dosis as $dosi) {
+            $dosi_id = $dosi->id;
+            $sql = "SELECT * FROM droga_x_dosis WHERE dosis_id = $dosi_id";
             $stmt = $db->query($sql);
-            $dosis = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $drogas = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-foreach($dosis as $dosi){
-$dosi_id = $dosi->id;
-  $sql = "SELECT * FROM droga_x_dosis WHERE dosis_id = $dosi_id";
-  $stmt = $db->query($sql);
-  $drogas = $stmt->fetchAll(PDO::FETCH_OBJ);
+            foreach ($drogas as $droga) {
+                $droga_id = $droga->droga_id;
+                $sql = "SELECT * FROM droga WHERE id = $droga_id";
+                $stmt = $db->query($sql);
+                $drogas_de_dosis = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-foreach($drogas as $droga){
-  $droga_id = $droga->id;
-  $sql = "SELECT * FROM droga WHERE id = $droga_id";
-  $stmt = $db->query($sql);
-  $drogas_de_dosis = $stmt->fetchAll(PDO::FETCH_OBJ);
+                $droga->nombre = $drogas_de_dosis[0]->nombre;
+            }
 
-  $droga->nombre = $drogas_de_dosis[0]->nombre;
-}
-
-  $dosi->drogas = $drogas;
-}
+            $dosi->drogas = $drogas;
+        }
 
 
-            $pastillero->dosis = $dosis;
+        $pastillero->dosis = $dosis;
 
 
         // Reset all variables
