437488db8213af126b034e48b38fc88b0835fa39
federicodonner <federico.donner@gmail.com>
2020-05-03 14:58:39 -0300

    feat(stock): return days of stock for each drug

--- src/routes/stock.php
+++ src/routes/stock.php
@@ -24,7 +24,7 @@ $app->get('/api/stock', function (Request $request, Response $response) {
         // Selecciona todas las drogas
         $stmt = $db->query($sql);
         $drogas = $stmt->fetchAll(PDO::FETCH_OBJ);
-        
+
         //Encuentra las medicinas que tienen la droga seleccionada
         foreach ($drogas as $droga) {
 
@@ -33,6 +33,37 @@ $app->get('/api/stock', function (Request $request, Response $response) {
             $sql = "SELECT * FROM stock WHERE droga = $droga_id";
             $stmt = $db->query($sql);
             $stocks = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+            $droga_mg_total = 0;
+
+            // Calcula para cuántos días hay stock
+            foreach ($stocks as $stock) {
+                $stockComprimido = $stock->comprimido;
+                $stockCantidad = $stock->cantidad;
+
+                // droga_mg_total son los miligramos totales disponibles de la medicina
+                $droga_mg_total = $droga_mg_total + $stockComprimido*$stockCantidad;
+            }
+            // Obtiene cuántos mg debe tomar el paciente por día
+            $sql = "SELECT cantidad_mg FROM droga_x_dosis WHERE droga_id = $droga_id";
+            $stmt = $db->query($sql);
+            $dosis_diaria = $stmt->fetchAll(PDO::FETCH_OBJ);
+            // Suma todas las dosis diarias de la droga
+            $dosis_total = 0;
+            foreach ($dosis_diaria as $dosis) {
+                $dosis_total = $dosis_total + $dosis->cantidad_mg;
+            }
+
+
+            $droga->disponible_total = $droga_mg_total;
+            $droga->dosis_total = $dosis_total;
+            if ($dosis_total != 0) {
+                $dias_disponible = floor($droga_mg_total/$dosis_total);
+            } else {
+                $dias_disponible = -1;
+            }
+
+            $droga->dias_disponible = $dias_disponible;
 
             $droga->stocks = $stocks;
         }
