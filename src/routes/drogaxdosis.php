956eb55de112a662d6d7d933e0d353a699666141
federicodonner <federico.donner@gmail.com>
2020-08-18 02:57:20 -0300

    feat(drogaxdosis): mejorar verificación de put y delete

--- src/routes/drogaxdosis.php
+++ src/routes/drogaxdosis.php
@@ -22,7 +22,6 @@ $app->post('/api/drogaxdosis', function (Request $request, Response $response) {
                 // Verifica que se hayan enviado los campos correctos
                 if ($droga_id && $dosis_id && $cantidad_mg) {
 
-
                     // Verifica que la droga exista
                     $sql="SELECT * FROM droga WHERE id = $droga_id";
 
@@ -75,15 +74,18 @@ $app->post('/api/drogaxdosis', function (Request $request, Response $response) {
                                 }
                             } else {  // if ($permisos_usuario->acceso_lectura_pastillero) {
                                 $db = null;
-                                return messageResponse($response, 'No tiene permisos para acceder al pastillero seleccionado', 403);
+                                return messageResponse($response, 'No tiene permisos para editar el pastillero seleccionado', 403);
                             }
                         } else {  // if (count($drogas)>0) {
+                            $db = null;
                             return messageResponse($response, 'La toma seleccionada no existe', 404);
                         }
                     } else {  // if (count($drogas)>0) {
+                        $db = null;
                         return messageResponse($response, 'La droga seleccionada no existe', 404);
                     }
                 } else {   //   if($droga_id && $dosis_id && $cantidad_mg){
+                    $db = null;
                     return messageResponse($response, 'Campos incorrectos', 401);
                 }
             } else {  // if (!empty($user_found)) {
@@ -103,68 +105,218 @@ $app->post('/api/drogaxdosis', function (Request $request, Response $response) {
 
 // Actualizar drogaxdosis
 $app->put('/api/drogaxdosis/{id}', function (Request $request, Response $response) {
-    $id = $request->getAttribute('id');
+    // Verify if the auth header is available
+    if ($request->getHeaders()['HTTP_AUTHORIZATION']) {
+        // If the header is available, get the token
+        $access_token = $request->getHeaders()['HTTP_AUTHORIZATION'][0];
+        $access_token = explode(" ", $access_token)[1];
+        // Find the access token, if a user is returned, post the products
+        if (!empty($access_token)) {
+            $user_found = verifyToken($access_token);
+            // Verify that there is a user logged in
+            if (!empty($user_found)) {
+                $id = $request->getAttribute('id');
 
-    $droga_id = $request->getParam('droga_id');
-    $dosis_id = $request->getParam('dosis_id');
-    $cantidad_mg = $request->getParam('cantidad_mg');
-    $notas = $request->getParam('notas');
+                // Verifica que la droga_x_dosis exista
+                $sql="SELECT * FROM droga_x_dosis WHERE id = $id";
+
+                $db = new db();
+                $db = $db->connect();
+
+                $stmt = $db->query($sql);
+                $drogaxdosises = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+                if (count($drogaxdosises)>0) {
+
+                    // Guarda los datos de la drogaxdosis anterior para los que no cambian
+                    $droga_id_anterior = $drogaxdosises[0]->droga_id;
+                    $dosis_id_anterior = $drogaxdosises[0]->dosis_id;
+                    $cantidad_mg_anterior = $drogaxdosises[0]->cantidad_mg;
+                    $notas = $drogaxdosises[0]->notas;
+
+                    // Obtiene el id de la droga para obtener el del pastillero
+                    // Y verificar que el usuario tenga permisos de edición
+                    $droga_id = $drogaxdosises[0]->droga_id;
+
+                    $sql = "SELECT * FROM droga WHERE id = $droga_id";
+                    $stmt = $db->query($sql);
+                    $drogas = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+                    $pastillero_id = $drogas[0]->pastillero_id;
+                    $usuario_id = $user_found[0]->usuario_id;
+
+                    $permisos_usuario = verificarPermisosUsuarioPastillero($usuario_id, $pastillero_id);
+
+                    // Como es un DELETE verifica permisos de escritura
+                    if ($permisos_usuario->acceso_edicion_pastillero) {
+                        // Variable para verificar si se especificó algún cambio
+                        $algun_cambio = false;
+                        // Si se quiere cambiar la droga, se verifica que exista
+                        $droga_id = $request->getParam('droga_id');
+                        if ($droga_id) {
+                            $algun_cambio = true;
+                            $sql="SELECT * FROM droga WHERE id = $droga_id";
+                            $stmt = $db->query($sql);
+                            $drogas = $stmt->fetchAll(PDO::FETCH_OBJ);
+                            if (count($drogas)==0) {
+                                $db = null;
+                                return messageResponse($response, 'Droga no encontrada', 404);
+                            }
+                        } else {
+                            $droga_id = $droga_id_anterior;
+                        }
+
+                        // Si se quiere cambiar la dosis, se verifica que exista
+                        $dosis_id = $request->getParam('dosis_id');
+                        if ($dosis_id) {
+                            $algun_cambio = true;
+                            $sql="SELECT * FROM dosis WHERE id = $dosis_id";
+                            $stmt = $db->query($sql);
+                            $dosises = $stmt->fetchAll(PDO::FETCH_OBJ);
+                            if (count($dosises)==0) {
+                                $db = null;
+                                return messageResponse($response, 'Dosis no encontrada', 404);
+                            }
+                        } else {
+                            $dosis_id = $dosis_id_anterior;
+                        }
 
 
-    $sql = "UPDATE droga_x_dosis SET
-        droga_id = :droga_id,
-        dosis_id = :dosis_id,
-        cantidad_mg = :cantidad_mg,
-        notas = :notas
-        WHERE id = $id";
+                        $cantidad_mg = $request->getParam('cantidad_mg');
+                        if ($cantidad_mg) {
+                            $algun_cambio = true;
+                        } else {
+                            $cantidad_mg = $cantidad_mg_anterior;
+                        }
 
-    try {
-        $db = new db();
-        $db = $db->connect();
 
-        $stmt = $db->prepare($sql);
+                        $notas = $request->getParam('notas');
+                        if ($notas) {
+                            $algun_cambio = true;
+                        } else {
+                            $notas = $notas_anterior;
+                        }
 
-        $stmt->bindParam(':droga_id', $droga_id);
-        $stmt->bindParam(':dosis_id', $dosis_id);
-        $stmt->bindParam(':cantidad_mg', $cantidad_mg);
-        $stmt->bindParam(':notas', $notas);
 
-        $stmt->execute();
+                        if ($algun_cambio) {
+                            $sql = "UPDATE droga_x_dosis SET
+                            droga_id = :droga_id,
+                            dosis_id = :dosis_id,
+                            cantidad_mg = :cantidad_mg,
+                            notas = :notas
+                            WHERE id = $id";
 
+                            try {
+                                $stmt = $db->prepare($sql);
+
+                                $stmt->bindParam(':droga_id', $droga_id);
+                                $stmt->bindParam(':dosis_id', $dosis_id);
+                                $stmt->bindParam(':cantidad_mg', $cantidad_mg);
+                                $stmt->bindParam(':notas', $notas);
+
+                                $stmt->execute();
+
+                                $db = null;
+                                return messageResponse($response, 'Dosis actualizada exitosamente.', 200);
+                            } catch (PDOException $e) {
+                                $db = null;
+                                return messageResponse($response, $e->getMessage(), 503);
+                            }
+                        } else {   //  if($algun_cambio){
+                            $db = null;
+                            return messageResponse($response, 'Campos incorrectos', 401);
+                        }
+                    } else {  // if ($permisos_usuario->acceso_lectura_pastillero) {
+                        $db = null;
+                        return messageResponse($response, 'No tiene permisos para editar el pastillero seleccionado', 403);
+                    }
+                } else {  //   if (count($dosises)>0) {
+                    $db = null;
+                    return messageResponse($response, 'La dosis seleccionada no existe', 404);
+                }
+            } else {  // if (!empty($user_found)) {
+                $db = null;
+                return messageResponse($response, 'Error de login, usuario no encontrado', 401);
+            }
+        } else { // if (!empty($access_token)) {
+            $db = null;
+            return messageResponse($response, 'Error de login, falta access token', 401);
+        }
+    } else { // if ($request->getHeaders()['HTTP_AUTHORIZATION']) {
         $db = null;
-        return messageResponse($response, 'Dosis actualizada exitosamente.', 200);
-    } catch (PDOException $e) {
-        $db = null;
-        return messageResponse($response, $e->getMessage(), 503);
+        return messageResponse($response, 'Error de encabezado HTTP', 401);
     }
 });
 
 // Eliminar drogaxdosis
 $app->delete('/api/drogaxdosis/{id}', function (Request $request, Response $response) {
-    $id = $request->getAttribute('id');
+    // Verify if the auth header is available
+    if ($request->getHeaders()['HTTP_AUTHORIZATION']) {
+        // If the header is available, get the token
+        $access_token = $request->getHeaders()['HTTP_AUTHORIZATION'][0];
+        $access_token = explode(" ", $access_token)[1];
+        // Find the access token, if a user is returned, post the products
+        if (!empty($access_token)) {
+            $user_found = verifyToken($access_token);
+            // Verify that there is a user logged in
+            if (!empty($user_found)) {
+                $id = $request->getAttribute('id');
 
-    $droga_id = $request->getParam('droga_id');
-    $dosis_id = $request->getParam('dosis_id');
-    $cantidad_mg = $request->getParam('cantidad_mg');
-    $notas = $request->getParam('notas');
+                // Verifica que la droga_x_dosis exista
+                $sql = "SELECT * FROM droga_x_dosis WHERE id = $id";
+                $db = new db();
+                $db = $db->connect();
 
+                $stmt = $db->query($sql);
+                $drogaxdosises = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-    $sql = "DELETE FROM droga_x_dosis WHERE id = $id";
+                if (count($drogaxdosises)>0) {
 
-    try {
-        // Get db object
-        $db = new db();
-        // Connect
-        $db = $db->connect();
+                    // Obtiene el id de la droga para obtener el del pastillero
+                    // Y verificar que el usuario tenga permisos de edición
+                    $droga_id = $drogaxdosises[0]->droga_id;
 
-        $stmt = $db->prepare($sql);
+                    $sql = "SELECT * FROM droga WHERE id = $droga_id";
+                    $stmt = $db->query($sql);
+                    $drogas = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-        $stmt->execute();
+                    $pastillero_id = $drogas[0]->pastillero_id;
+                    $usuario_id = $user_found[0]->usuario_id;
 
+                    $permisos_usuario = verificarPermisosUsuarioPastillero($usuario_id, $pastillero_id);
+
+                    // Como es un DELETE verifica permisos de escritura
+                    if ($permisos_usuario->acceso_edicion_pastillero) {
+                        $sql = "DELETE FROM droga_x_dosis WHERE id = $id";
+
+                        try {
+                            $stmt = $db->prepare($sql);
+                            $stmt->execute();
+
+                            $db = null;
+                            return messageResponse($response, "Dosis eliminada exitosamente.", 200);
+                        } catch (PDOException $e) {
+                            $db = null;
+                            return messageResponse($response, $e->getMessage(), 503);
+                        }
+                    } else {  // if ($permisos_usuario->acceso_lectura_pastillero) {
+                        $db = null;
+                        return messageResponse($response, 'No tiene permisos para editar el pastillero seleccionado', 403);
+                    }
+                } else {  // if (count($drogaxdosises)>0) {
+                    $db = null;
+                    return messageResponse($response, 'La toma seleccionada no existe', 404);
+                }
+            } else {  // if (!empty($user_found)) {
+                $db = null;
+                return messageResponse($response, 'Error de login, usuario no encontrado', 401);
+            }
+        } else { // if (!empty($access_token)) {
+            $db = null;
+            return messageResponse($response, 'Error de login, falta access token', 401);
+        }
+    } else { // if ($request->getHeaders()['HTTP_AUTHORIZATION']) {
         $db = null;
-        return messageResponse($response, "Dosis eliminada exitosamente.", 200);
-    } catch (PDOException $e) {
-        $db = null;
-        return messageResponse($response, $e->getMessage(), 503);
+        return messageResponse($response, 'Error de encabezado HTTP', 401);
     }
 });
